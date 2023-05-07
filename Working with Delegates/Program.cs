a1b6948d3e0d1961f9b0684039d457121b1f7236
Rayna Miller <89815185+UsernameI23@users.noreply.github.com>
2023-05-06 21:29:58 -0400

    Update Program.cs

--- Working with Delegates/Program.cs
+++ Working with Delegates/Program.cs
@@ -1,9 +1,9 @@
-﻿namespace Assignment8ex2
+namespace Assignment8ex2
 {
-    public delegate void myDelegates();
+    public delegate double myDelegate(double x, double y);
     public class MathSolutions
     {
-        public delegate void myDelegate();
+        //public delegate void myDelegate();
         public double GetSum(double x, double y)
         {
             return x + y;
@@ -21,18 +21,26 @@
             else
                 Console.WriteLine($" {b} is equal to {a}");
         }
+        
+        
         static void Main(string[] args)
         {
             // create a class object
-            
+           
+
             MathSolutions answer = new MathSolutions();
             Random r = new Random();
             double num1 = Math.Round(r.NextDouble() * 100);
             double num2 = Math.Round(r.NextDouble() * 100);
-            Action<int, int> calcResult = delegate (int x, int y) { Console.WriteLine($" {num1} + {num2} = {answer.GetSum(num1, num2)}"); };
-            Func<int, int, int> calcResult2 = delegate (int a, int b) { return a * b; }; Console.WriteLine($" {num1} * {num2} = {answer.GetProduct(num1, num2)}");
-            
-            answer.GetSmaller(num1, num2);
+
+            Action<double, double> calcResult = answer.GetSmaller;
+            calcResult(num1, num2);
+            Func<double, double, double> calcResult2 = answer.GetProduct;
+            Console.WriteLine($"{num1} x {num2} = {calcResult2(num1, num2)}");
+
+            myDelegate calcResult3 = new myDelegate(answer.GetSum);
+            Console.WriteLine($"{num1} + {num2} = {calcResult3(num1, num2)}");
+            //answer.GetSmaller(num1, num2);
            
 
         }
